549b6e74e5f46e83bef68b5dce72dafdf4cf78dc
Shaikh-Yusuf <shaikh.yusuf.arif@gmail.com>
2025-01-19 23:43:45 -0800

    Lessons Added Till 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule,MatMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/lesson14/lesson14.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson14',
+  imports: [],
+  templateUrl: './lesson14.component.html',
+  styleUrl: './lesson14.component.css'
+})
+export class Lesson14Component {
+
+}
